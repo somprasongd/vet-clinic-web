6b9ddad8e9128b71599704c764b962b0c221762b
Poom <poompongphun@gmail.com>
2020-09-16 15:21:28 +0700

    change ssr to spa

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,12 +5,12 @@ export default {
    ** Nuxt rendering mode
    ** See https://nuxtjs.org/api/configuration-mode
    */
-  ssr: true,
+  ssr: false,
   /*
    ** Nuxt target
    ** See https://nuxtjs.org/api/configuration-target
    */
-  target: 'server',
+  target: 'static',
   /*
    ** Headers of the page
    ** See https://nuxtjs.org/api/configuration-head
