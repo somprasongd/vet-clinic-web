ae9e9a3e984952c7dbfc486ae2c9316aaf4c91be
Poom <poompongphun@gmail.com>
2020-09-18 14:29:34 +0700

    Update router.base

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,6 +11,9 @@ export default {
    ** See https://nuxtjs.org/api/configuration-target
    */
   target: 'static',
+  router: {
+    base: '/vet-clinic-web/',
+  },
   /*
    ** Headers of the page
    ** See https://nuxtjs.org/api/configuration-head
