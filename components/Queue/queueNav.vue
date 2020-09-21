e68d5adde07b3eb4c5f5339aa42ccc4b66483a53
Poom <poompongphun@gmail.com>
2020-09-21 10:50:46 +0700

    Update New second nav style for responsive

--- components/Queue/queueNav.vue
+++ components/Queue/queueNav.vue
@@ -1,5 +1,56 @@
 <template>
-  <v-row class="cusblue1 second-nav" align="center" justify="center" dense>
+  <v-card color="grey lighten-4" flat tile>
+    <v-row
+      class="second-nav cusblue1"
+      height="55"
+      align="center"
+      justify="center"
+      dense
+      flat
+    >
+      <h1 class="font-weight-medium col-sm-2 col-12">คิว ({{ queueCount }})</h1>
+
+      <v-spacer></v-spacer>
+
+      <v-subheader
+        v-show="!this.$vuetify.breakpoint.smAndDown"
+        class="cus-subhead"
+        >ประเภทการตรวจ :</v-subheader
+      >
+      <v-select
+        v-model="type"
+        class="rounded-lg cus-input mr-3"
+        background-color="cusblue3"
+        :items="typeCheck"
+        dark
+        filled
+        rounded
+        single-line
+        dense
+        flat
+        hide-details
+      ></v-select>
+      <v-subheader
+        v-show="!this.$vuetify.breakpoint.smAndDown"
+        class="cus-subhead"
+        >แพทย์ผู้ตรวจ :</v-subheader
+      >
+      <v-select
+        class="rounded-lg cus-input"
+        background-color="cusblue3"
+        :items="doct"
+        label="ทั้งหมด"
+        dark
+        filled
+        rounded
+        single-line
+        dense
+        flat
+        hide-details
+      ></v-select>
+    </v-row>
+  </v-card>
+  <!-- <v-row class="cusblue1 second-nav" align="center" justify="center" dense>
     <v-col md="6" cols="12">
       <h1 class="font-weight-medium">คิว ({{ queueCount }})</h1>
     </v-col>
@@ -53,7 +104,7 @@
         </v-col>
       </v-row>
     </v-col>
-  </v-row>
+  </v-row> -->
 </template>
 
 <script>
