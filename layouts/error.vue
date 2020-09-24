a5a0b11af5c0ff518912173b7615559984fc5365
Poom <poompongphun@gmail.com>
2020-09-24 15:03:48 +0700

    Add error page

---
+++ layouts/error.vue
@@ -0,0 +1,52 @@
+<template>
+  <v-app dark>
+    <v-container class="fill-height" fluid>
+      <v-row align="center" justify="center">
+        <v-col cols="12">
+          <v-card class="text-center" color="transparent" flat>
+            <div class="display-3 mt-5">
+              <span>
+                {{ error.statusCode === 404 ? pageNotFound : otherError }}
+              </span>
+            </div>
+            <v-btn color="primary" to="/" text>Back to homepage</v-btn>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-app>
+</template>
+
+<script>
+export default {
+  layout: 'LoginLayout',
+  props: {
+    error: {
+      type: Object,
+      default: null,
+    },
+  },
+  data() {
+    return {
+      pageNotFound: '404 Not Found',
+      otherError: 'An error occurred',
+    }
+  },
+  head() {
+    const title =
+      this.error.statusCode === 404 ? this.pageNotFound : this.otherError
+    return {
+      title,
+    }
+  },
+}
+</script>
+
+<style scoped>
+h1 {
+  font-size: 20px;
+}
+.paragraph-text {
+  font-size: 18px;
+}
+</style>
