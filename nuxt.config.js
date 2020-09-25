7778e4e106c51ee00de5ca9202a47bb6b7602377
Poom <poompongphun@gmail.com>
2020-09-25 17:54:46 +0700

    edit baseurl

--- nuxt.config.js
+++ nuxt.config.js
@@ -84,7 +84,7 @@ export default {
    ** See https://axios.nuxtjs.org/options
    */
   axios: {
-    baseURL: process.env.API_URL,
+    baseURL: process.env.API_URI,
   },
   /*
    ** Auth module configuration
