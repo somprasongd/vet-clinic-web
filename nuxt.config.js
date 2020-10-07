eeaca3f80ca323ea8a2cbcdd4122493c7c455d82
Poom <poompongphun@gmail.com>
2020-10-07 13:50:26 +0700

    update viewport

--- nuxt.config.js
+++ nuxt.config.js
@@ -24,7 +24,11 @@ export default {
     title: process.env.npm_package_name || 'VetClinic',
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      {
+        name: 'viewport',
+        content:
+          'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0',
+      },
       {
         hid: 'description',
         name: 'description',
